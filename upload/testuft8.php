e41c70faf3961077c63f2cd951c594b536970e3b
lihong <1091853304@qq.com>
2017-04-17 13:39:10 +0800

    文件上传，80%

---
+++ upload/testuft8.php
@@ -0,0 +1,11 @@
+<?php
+// 允许上传的图片后缀
+echo "<meta http-equiv='Content-Type'' content='text/html; charset=utf-8'>";
+
+header("content-type:text/html,charset=utf-8");
+// header("Content-type: text/html; charset=gb2312");
+echo "哈喽";
+
+
+
+?>
